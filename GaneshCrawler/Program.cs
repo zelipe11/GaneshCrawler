6a073f4f6a73af21e5f69432fdde99b5d83cd196
Tiago <tiago.ramos@festpan.com.br>
2024-02-23 09:55:53 -0300

    Alterações

--- GaneshCrawler/Program.cs
+++ GaneshCrawler/Program.cs
@@ -2,96 +2,148 @@
 using Dapper;
 using HtmlAgilityPack;
 using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Web;
-using System.Xml;
+using System.Threading;
+using System.Net.Http;
 
 Console.WriteLine("Iniciando crawler...");
 
-HtmlWeb web = new()
+var web = new HtmlWeb
 {
     UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 };
 
-HtmlDocument doc = await web.LoadFromWebAsync("http://ganesh.easyorder.com.br/clientes?page=1");
-Console.WriteLine("Home obtida...");
-
-var categories = doc.DocumentNode.SelectNodes("//div[contains(@class, 'table table-striped no-border')]/tbody/tr");
-Console.WriteLine("Categorias obtida...");
-
-Console.WriteLine("Abrindo conexão com banco de dados...");
-
-var connection = new MySqlConnection("Server=localhost;Port=3306;DataBase=condo;Uid=root;Pwd=123456;");
-
-foreach (var category in categories)
+// Criar um contêiner de cookies para manter a sessão
+var cookieContainer = new CookieContainer();
+web.PreRequest += request =>
 {
-    string hrefPattern = @"<a\s+(?:[^>]*?\s+)?href=(['""])(.*?)\1";
-    Match hrefMatch = Regex.Match(category.InnerHtml, hrefPattern);
-
-    string textPattern = @"<a[^>]*>(.*?)<\/a>";
-    Match textMatch = Regex.Match(category.InnerHtml, textPattern);
-
-    if (hrefMatch.Success && textMatch.Success)
+    if (request is HttpWebRequest httpRequest)
     {
-        string categoryLink = hrefMatch.Groups[2].Value;
-        string categoryName = textMatch.Groups[1].Value;
+        httpRequest.CookieContainer = cookieContainer;
+    }
+    return true;
+};
 
-        var categoryAlreadyExist = connection.ExecuteScalar<bool>("SELECT COUNT(1) FROM Category WHERE Name=@text", new { text = categoryName });
+using (var httpClient = new HttpClient(new HttpClientHandler { CookieContainer = cookieContainer }))
+{
+    // Adicione o token XSRF diretamente ao contêiner de cookies
+    cookieContainer.Add(new Uri("http://ganesh.easyorder.com.br"), new Cookie("XSRF-TOKEN", "eyJpdiI6Im11SlBlaFRtalBwOGlQQzZxb0I4Wmc9PSIsInZhbHVlIjoiUmVQdnpodGNxVnlLQ1Nxc1MzRlhETmpwUmo3UUJIcmJ3WnRFRCt4MDdJQTdyTHJqQXBxTnE4OXVGMmdwWE1Hb0crcnJDemF1RnowK0FvYUo1Sk8zRWhOMzkrcHBpdnhDVVBUUHIxTHQ0MDg2Q0xSRGNvVVhCSmRzVWFNa0RtcjgiLCJtYWMiOiI4MTM0MWFlOWY5NTYwMGU3M2MxMzIyNjc5ZTAxZDI3N2I5ZjVkYjNlMjY1M2Q3M2FmMmU5YzM2NjU5NjNkYTk3In0%3D"));
 
-        if (categoryAlreadyExist is false)
+    // Agora, você pode realizar solicitações subsequentes usando HttpClient
+    var response = httpClient.GetAsync("http://ganesh.easyorder.com.br/clientes?page=1").Result;
+
+    // Se o login inicial também exigir um token CSRF, certifique-se de incluí-lo nos cabeçalhos da solicitação
+    // var tokenCSRF = "seu_token_csrf_aqui";
+    // httpClient.DefaultRequestHeaders.Add("X-CSRF-Token", tokenCSRF);
+
+    if (response.IsSuccessStatusCode)
+    {
+        var content = response.Content.ReadAsStringAsync().Result;
+        var doc = new HtmlDocument();
+        doc.LoadHtml(content);
+
+        // Verificar se o login foi bem-sucedido
+        if (cookieContainer.Count > 0)
         {
-            var sql = "INSERT INTO Category (Name, Url) VALUES (@name, @url)";
-            var newCategory = new Category { Name = categoryName, Url = categoryLink };
+            Console.WriteLine("Login bem-sucedido...");
 
-            var rowsAffected = connection.Execute(sql, newCategory);
+            // Agora você pode prosseguir com a obtenção do documento após o login
+            var doc2 = web.Load("http://ganesh.easyorder.com.br/clientes");
+            Console.WriteLine("Home obtida...");
 
-            if (rowsAffected > 0)
-                Console.WriteLine($"{categoryName} - categoria inserida com sucesso no banco...");
-            else
-                Console.WriteLine($"erro ao inserir categoria {categoryName}...");
+            var form = doc2.DocumentNode.SelectSingleNode("//form[@id='form1']");
+            var categoriesContainer = form?.SelectSingleNode(".//div[@id='table']//tbody");
+            var categories = categoriesContainer?.SelectNodes(".//tr");
 
-            Thread.Sleep(1000);
-        }
+            Console.WriteLine("Categorias obtidas...");
 
-        Console.WriteLine($"Obtendo empresas da categoria {categoryName}...");
+            Console.WriteLine("Abrindo conexão com banco de dados...");
 
-        var pageSize = 117;
+            var connection = new MySqlConnection("Server=localhost;Port=3306;DataBase=condo;Uid=root;Pwd=123456;");
 
-        HtmlDocument categoryDoc = await web.LoadFromWebAsync($"http://ganesh.easyorder.com.br/{categoryName}?page={pageSize}", Encoding.GetEncoding("iso-8859-1"));
-        var categoryTable = categoryDoc.DocumentNode.SelectNodes("//table//tbody//tr");
-
-        if (categoryTable != null)
-        {
-            foreach (HtmlNode node in categoryTable)
+            foreach (var category in categories)
             {
-                string name = node.SelectSingleNode("td[1]").InnerText.Trim();
-                string cnpj = node.SelectSingleNode("td[2]").NextSibling.InnerText.Trim();
-                string email = node.SelectSingleNode("td[3]").NextSibling.InnerText.Trim();
+                string hrefPattern = @"<a\s+(?:[^>]*?\s+)?href=(['""])(.*?)\1";
+                Match hrefMatch = Regex.Match(category.InnerHtml, hrefPattern);
 
-                var categoryId = connection.ExecuteScalar<int>("SELECT Id FROM Category WHERE Name=@text", new { text = categoryName });
+                string textPattern = @"<a[^>]*>(.*?)<\/a>";
+                Match textMatch = Regex.Match(category.InnerHtml, textPattern);
 
-                var sql = "INSERT INTO Client (Name, CNPJ/CPF, Email) " +
-                          "VALUES (@name, @cnpj, @email)";
-
-                var client = new Client
+                if (hrefMatch.Success && textMatch.Success)
                 {
-                    Name = name,
-                    Cnpjcpf = cnpj,
-                    Email = email
-                };
+                    string categoryLink = hrefMatch.Groups[2].Value;
+                    string categoryName = textMatch.Groups[1].Value;
 
-                var rowsAffected = connection.Execute(sql, client);
+                    var categoryAlreadyExist = connection.ExecuteScalar<bool>("SELECT COUNT(1) FROM Category WHERE Name=@text", new { text = categoryName });
 
-                if (rowsAffected > 0)
-                    Console.WriteLine($"{categoryName} - Empresa inserida com sucesso no banco...");
-                else
-                    Console.WriteLine($"erro ao inserir empresa {name}...");
+                    if (categoryAlreadyExist is false)
+                    {
+                        var sql = "INSERT INTO Category (Name, Url) VALUES (@name, @url)";
+                        var newCategory = new Category { Name = categoryName, Url = categoryLink };
 
-                Thread.Sleep(500);
+                        var rowsAffected = connection.Execute(sql, newCategory);
+
+                        if (rowsAffected > 0)
+                            Console.WriteLine($"{categoryName} - categoria inserida com sucesso no banco...");
+                        else
+                            Console.WriteLine($"erro ao inserir categoria {categoryName}...");
+
+                        Thread.Sleep(1000);
+                    }
+
+                    Console.WriteLine($"Obtendo empresas da categoria {categoryName}...");
+
+                    var pageSize = 117;
+
+                    HtmlDocument categoryDoc = await web.LoadFromWebAsync($"http://ganesh.easyorder.com.br/{categoryName}?page={pageSize}", Encoding.GetEncoding("iso-8859-1"));
+                    var categoryTable = categoryDoc.DocumentNode.SelectNodes("//table//tbody//tr");
+
+                    if (categoryTable != null)
+                    {
+                        foreach (HtmlNode node in categoryTable)
+                        {
+                            string name = node.SelectSingleNode("td[1]").InnerText.Trim();
+                            string cnpj = node.SelectSingleNode("td[2]").NextSibling.InnerText.Trim();
+                            string email = node.SelectSingleNode("td[3]").NextSibling.InnerText.Trim();
+
+                            var categoryId = connection.ExecuteScalar<int>("SELECT Id FROM Category WHERE Name=@text", new { text = categoryName });
+
+                            var sql = "INSERT INTO Client (Name, CNPJ/CPF, Email) " +
+                                      "VALUES (@name, @cnpj, @email)";
+
+                            var client = new Client
+                            {
+                                Name = name,
+                                Cnpjcpf = cnpj,
+                                Email = email
+                            };
+
+                            var rowsAffected = connection.Execute(sql, client);
+
+                            if (rowsAffected > 0)
+                                Console.WriteLine($"{categoryName} - Empresa inserida com sucesso no banco...");
+                            else
+                                Console.WriteLine($"erro ao inserir empresa {name}...");
+
+                            Thread.Sleep(500);
+                        }
+                    }
+
+                    Thread.Sleep(10000);
+                }
             }
         }
-
-        Thread.Sleep(10000);
+        else
+        {
+            Console.WriteLine("Falha no login. Verifique as credenciais.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Falha na solicitação: {response.StatusCode}");
     }
 }
